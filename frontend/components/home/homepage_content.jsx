7332510e8d8ecd741ab1644fd08a8bbde2329132
Arwen <arwensookim@gmail.com>
2021-09-29 12:18:48 -0700

    main homepage upadated

--- frontend/components/home/homepage_content.jsx
+++ frontend/components/home/homepage_content.jsx
@@ -7,14 +7,41 @@ class Homepage extends React.Component{
         return(
             <div className="homepage">
                 <div className="bar">
-                    <p className="text">never ever make your life messy</p>
+                    <p className="main-text">never ever make your life messy</p>
                     <button className="download-button" >Downlaod</button>
                 </div>
 
                 <div className="main-top">
                     <h1>Power of NeverEverNote, Make your life Organize</h1>
-                    <h2>Try NeverEverNote today</h2>
+                    <p>Remembr everything and tackle any project with your notes, tasks, and shedule all in one place.</p>
+                    <Link to='/signup'>
+                        <button className="signup-button" >Sign up for free</button>
+                    </Link>
+                </div>
+
+                <div className="main-middle">
                     <img className="homepage-image" src={window.homepageURL} />
+                    <div className="middle-texts">
+                        <div className="middle-text">
+                            <h3>WORK ANYWHERE</h3>
+                            <p>You can log in to your account from any web browser!</p>
+                        </div>
+
+                        <div className="middle-text">
+                            <h3>REMEMBER EVERYTHING</h3>
+                            <p>You can keep track of your notes!</p>
+                        </div>
+
+                        <div className="middle-text">
+                            <h3>ORGANIZE YOUR NOTE</h3>
+                            <p>Keep your same categorizes notes under a notebook!</p>
+                        </div>
+
+                        <div className="middle-text">
+                            <h3>FIND THINGS FAST</h3>
+                            <p>You can easily sort your content with tags!</p>
+                        </div>  
+                    </div>
                 </div>
 
             </div>
