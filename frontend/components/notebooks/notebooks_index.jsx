a4f07e53ad3bad6589aeb036f70a3e50377368b9
Arwen <arwensookim@gmail.com>
2021-10-05 17:33:28 -0700

    notebooks_index file updated

--- frontend/components/notebooks/notebooks_index.jsx
+++ frontend/components/notebooks/notebooks_index.jsx
@@ -103,20 +103,45 @@ class NotebooksIndex extends React.Component{
                 <ul className="notebooks-index-list">
                     <li><h3>TITLE</h3></li>
                     <li><h3>CREATED BY</h3></li>
-                </ul>
-                {this.props.notebooks.map( notebook => (
-                    <li key={notebook.id}>
-                        <div className="notebook-list-title">
-                            <Link to={`/notebooks/${notebook.id}/notes`}>{notebook.title}</Link>
-                        </div>
 
-                        <div className="notebook-list-created-by">
-                            {this.props.currentUser.username}
+                    {this.props.notebooks.map( notebook => (
+                        <li key={notebook.id}>
+                            <div className="notebook-list-title">
+                                <Link to={`/notebooks/${notebook.id}/notes`}>{notebook.title}</Link>
+                            </div>
+
+                            <div className="notebook-list-created-by">
+                                {this.props.currentUser.username}
+                            </div>
+                            <button className="rename-title" onClick={ () =>{this.handleOpenRenameModal(notebook)} }>Edit Title</button>
+                            <button className="delete-notebook" onClick={ () => { this.handleDeleteNotebook(notebook.id)}}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+
+                <Modal isOpen={this.state.modal}>
+                    <div>
+                        <h2 className="modal-header">Rename Notebook Title</h2>
+                        <label className="modal-title">Title</label>
+                        <input type="text" className="rename-title-input" placeholder="Title" value={this.state.renamedNotebook} onChange={this.update('renamedNotebook')} />
+                        <div className="modal-button">
+                            <button className="cancel" onClick={this.handleCloseModal}>Cancel</button>
+                            <button className="save" onClick={this.handleRename}>Save</button>
                         </div>
-                        <button className="edit-title" onClick={ () =>{this.handleEditTitle(notebook)} }>Edit Title</button>
-                        <button className="delete-notebook" onClick={ () => { this.handleDeleteNotebook(notebook.id)}}>Delete</button>
-                    </li>
-                ))}
+                    </div>
+                </Modal>
+
+                <Modal isOpen={this.state.notebookModal}>
+                    <div>
+                        <h2>Create New Notebook</h2>
+                        <label className="create-notebook-title-modal">Title</label>
+                        <input type="text" className="create-notebook-input-modal" placeholder="Title" value={this.state.title} onChange={this.update('title')}/>
+                        <div>
+                            <button onClick={this.handleCloseNotebookModal}>Cancel</button>
+                            <button onClick={this.handleCreateNotebook}>Create</button>
+                        </div>
+                    </div>
+                </Modal>
             </div>
         )
     }
