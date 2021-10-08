af1c3ef007d9c860fd25cd65dd2fbf0d5f7700a3
Arwen <arwensookim@gmail.com>
2021-10-07 19:09:51 -0700

    date util updated

--- frontend/util/date_util.js
+++ frontend/util/date_util.js
@@ -37,22 +37,10 @@ export const formatDate = date => {
   );
   
   export const formatDateNotebook = date => {
-    const months = {
-      0: 'Jan',
-      1: 'Feb',
-      2: 'Mar',
-      3: 'Apr',
-      4: 'May',
-      5: 'Jun',
-      6: 'Jul',
-      7: 'Aug',
-      8: 'Sep',
-      9: 'Oct',
-      10: 'Nov',
-      11: 'Dec',
-    };
+
     const obj = new Date(date);
-    const month = months[obj.getMonth()];
+    const month = obj.getMonth();
     const day = obj.getDate();
-    return `${month} ${day}`;
+    const year = obj.getFullYear();
+    return `${month}/${day}/${year} `;
   };
