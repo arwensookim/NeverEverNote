84d0b29eb812917e839b887f11e25868f39662a8
Arwen <arwensookim@gmail.com>
2021-10-07 21:30:23 -0700

    seeds file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,22 +14,56 @@ demo = User.create!(username: "demouser", password: "password")
 arwen = User.create!(username: "miknewra", password: "password123")
 
 notebook1 = Notebook.create!(
-    title: 'Coding',
+    title: 'Diary',
     user_id: demo.id,
 )
+
+
+notebook2 = Notebook.create!(
+    title: 'Test Notebook',
+    user_id: demo.id,
+)
+
 
 note1 = Note.create!(
-    title: 'My Fullstack Project',
-    body: 'NeverEverNote for my first fullstack project is not easy',
+    title:'September, 27th',
+    body:'Start my first Fullstack Project to cloning Evernote!',
+    user_id: demo.id,
+    notebook_id: notebook1.id
+)
+
+note2 = Note.create!(
+    title:'October, 7th',
+    body:'Finished most of the MVPs for the projects.',
     user_id: demo.id,
     notebook_id: notebook1.id
 )
 
 
-
-note2 = Note.create!(
-    title: 'hello',
-    body: 'hellohello',
+note3 = Note.create!(
+    title:'Test Note',
+    body:'This is Test Note',
     user_id: demo.id,
-    notebook_id: notebook1.id
+    notebook_id: notebook2.id
+)
+
+note4 = Note.create!(
+    title:'Test Note 2',
+    body:'This is another test note',
+    user_id: demo.id,
+    notebook_id: notebook2.id
+)
+
+note5 = Note.create!(
+    title:'What is my favorite Programming Language?',
+    body:'Ruby, Javascript is my favorite Programming Languages',
+    user_id: demo.id,
+    notebook_id: notebook2.id
+)
+
+note6 = Note.create!(
+    title: 'functional component',
+    body: 'I am still not used to using functional component',
+    user_id: demo.id,
+    notebook_id: notebook2.id
 )
