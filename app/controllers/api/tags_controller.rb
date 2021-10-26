3011ba87f2ec324f1e00fbe0a2a4f95c3d7d8245
Arwen <arwensookim@gmail.com>
2021-10-26 15:50:17 -0700

    create tag controller

---
+++ app/controllers/api/tags_controller.rb
@@ -0,0 +1,49 @@
+class Api::TagsController < ApplicationController
+    before_action :require_logged_in, only: [:create, :index, :update, :destroy, :show]
+
+
+    def index
+        @tags = current_user.tags
+        render :index
+    end
+
+    def show
+        @tag = Tag.find_by(id: params[:id])
+        render :show
+    end
+
+    def create
+        @tag = Tag.new(tag_params)
+        
+        if @tag.save
+            render :show
+        else
+            render json: @tag.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @tag = Tag.find_by(id: params[:id])
+        
+        if @tag.update(tag_params)
+            render :show
+        else
+            render json: @tag.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @tag = Tag.find_by(id: params[:id])
+
+        if @tag.destroy
+            render :show
+        else
+            render json: @tag.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    def tag_params
+        params.require(:tag).permit(:title, :user_id)
+    end
+end
