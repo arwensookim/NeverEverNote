d1815d6d62d1536f606e940472df7c275b56ee72
Arwen <arwensookim@gmail.com>
2021-10-27 11:52:59 -0700

    updates

--- frontend/components/tags/tags_index.jsx
+++ frontend/components/tags/tags_index.jsx
@@ -3,6 +3,21 @@ import React from "react";
 class TagsIndex extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            title: "",
+            modal: false,
+            tagmodal: false,
+            retitle: null,
+            retitleTag: ""
+        }
+    }
+
+    componentDidMount() {
+        this.props.fetchTags();
+    }
+
+    handleDelete(tagId) {
+        this.props.deleteTag(tagId);
     }
 
     render() {
