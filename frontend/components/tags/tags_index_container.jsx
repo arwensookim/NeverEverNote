6f643241e2558c9191776fa303fd8cfc5c7477a3
Arwen <arwensookim@gmail.com>
2021-10-27 10:58:19 -0700

    update tags index container

--- frontend/components/tags/tags_index_container.jsx
+++ frontend/components/tags/tags_index_container.jsx
@@ -1,2 +1,17 @@
 import { connect } from "react-redux";
 import { fetchTags, createTag, updateTag, deleteTag } from "../../actions/tag_actions";
+import TagsIndex from "./tags_index";
+
+const mSTP = state => ({
+    tags: Object.values(state.entities.tags),
+    currentUser: state.entities.users[state.session.id],
+})
+
+const mDTP = dispatch => ({
+    fetchTags: () => dispatch(fetchTags()),
+    createTag: tag => dispatch(createTag(tag)),
+    updateTag: tag => dispatch(updateTag(tag)),
+    deleteTag: tagId => dispatch(deleteTag(tagId))
+})
+
+export default connect(mSTP, mDTP)(TagsIndex);
