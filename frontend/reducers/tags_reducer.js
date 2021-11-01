cfff63940489d637389216146e080dbe096d67f5
Arwen <arwensookim@gmail.com>
2021-11-01 10:49:15 -0700

    update tag reducer

--- frontend/reducers/tags_reducer.js
+++ frontend/reducers/tags_reducer.js
@@ -2,17 +2,17 @@ import { RECEIVE_TAGS, RECEIVE_TAG, REMOVE_TAG } from "../actions/tag_actions";
 
 const tagsReducer = ( oldState = {}, action ) => {
     Object.freeze(oldState);
-    let nextState = Object.assign({}, oldState);
+    let newState = Object.assign({}, oldState);
 
     switch (action.type) {
         case RECEIVE_TAGS:
             return action.tags;
         case RECEIVE_TAG:
-            nextState[action.tag.id] = action.tag;
-            return nextState;
+            newState[action.tag.id] = action.tag;
+            return newState;
         case REMOVE_TAG:
-            delete nextState[action.tagId];
-            return nextState
+            delete newState[action.tagId];
+            return newState
     
         default:
             return oldState;
