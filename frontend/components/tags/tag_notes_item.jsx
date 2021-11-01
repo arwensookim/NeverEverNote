790089e364e15e6d7964300f0177b9cca3b3c757
Arwen <arwensookim@gmail.com>
2021-11-01 13:57:07 -0700

    update tag notes item

--- frontend/components/tags/tag_notes_item.jsx
+++ frontend/components/tags/tag_notes_item.jsx
@@ -3,8 +3,36 @@ import { Link } from "react-router-dom";
 import { formatDateTime } from "../../util/date_util";
 
 const TagNotesItem = props => {
+
+    let date = props.note.updated_at;
+
+    const currentDate = new Date();
+    const updatedDate = new Date(date);
+    let showDate;
+    if((currentDate.getDate() === updatedDate.getDate()) && (currentDate.getMonth() === updatedDate.getMonth())) {
+        if(currentDate.getMinutes() === updatedDate.getMinutes()) {
+            showDate = "a few seconds ago";
+        } else if (currentDate.getMinutes() - updatedDate.getMinutes() < 10) {
+            showDate = "a few minutes ago";
+        } else if (currentDate.getHours() === updatedDate.getHours()) {
+            let time = currentDate.getMinutes() - updatedDate.getMinutes();
+            showDate = `${time} minutes ago`;
+        } else {
+            showDate = formatDateTime(date);
+        }
+    } else {
+        showDate = formatDateTime(date);
+    }
+
+    let url = `/tags/${props.tagId}/${props.note.id}`
+    const body = props.note.body.slice(0,150).replace(/<[^>]*>?/gm, '');
     return(
-        <Link>
+        <Link to={url}>
+            <li className="notes-lists">
+                <div className="note-title">{props.note.title}</div>
+                <div className="note-body">{body}</div>
+                <div className="note-datetime">{showDate}</div>
+            </li>
         </Link>
     )
 }
