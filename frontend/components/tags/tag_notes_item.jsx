cb98ea2310f43e9043b4fa8740a1cda2bb6deddd
Arwen <arwensookim@gmail.com>
2021-11-01 13:54:44 -0700

    add tag notes item

---
+++ frontend/components/tags/tag_notes_item.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { formatDateTime } from "../../util/date_util";
+
+const TagNotesItem = props => {
+    return(
+        <Link>
+        </Link>
+    )
+}
+
+export default TagNotesItem;
