1f8a2beaaee279add2382b6c141ec6346f3da31c
Arwen <arwensookim@gmail.com>
2021-11-01 13:53:12 -0700

    add tag notes list

---
+++ frontend/components/tags/tag_notes_list.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+class TagNotesList extends React.Component{
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+        this.props.fetchNotes();
+    }
+
+    handleNewNote() {
+        let newNote = {
+            title: "",
+            body: "",
+            user_id: this.props.currentUser.id,
+            notebook_id: this.props.notebooks[0].id
+        }
+
+        this.props.createNote(newNote)
+            .then( res => this.props.history.push(`/notes/${res.note.id}`))
+    }
+
+    render() {
+        let notes = this.props.notes.sort()
+        const date = notes.map( ( note, i ) => [note.updateed_at, i]);
+        date.sort().reverse();
+
+        let sort_notes = [];
+        if (date){
+            for(let i = 0; i < date.length; i++){
+                let temp = date[i][1];
+                let temp_note = notes[temp];
+                sort_notes.push(temp_note);
+            }
+        }
+
+        if(sort_notes){
+            notes = sort_notes;
+        }
+
+        return(
+            <ul>
+                {notes.map(note => <TagNotesItem notebooks={this.props.notebooks} note={this.props.note} key={note.id} tagId={this.props.tagId} />)}
+            </ul>
+        )
+    }
+}
+
+export default TagNotesList;
